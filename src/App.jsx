3be0f78c4e9f76be5a4b64acef6197b584e95cbb
Scoutress <142579732+Scoutress@users.noreply.github.com>
2024-09-22 14:33:02 +0300

    Same changes for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import Router from "./navigation/Router";
-import "./styles/Main.module.scss";
+import "./styles/global.scss";
 
 const App = () => {
   return <RouterProvider router={Router} />;
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/styles/Variables.module.scss";`,
-      },
-    },
-  },
   plugins: [react()],
 });
